488ca92a7d21568e41080f995fb002b340c623ea
Vinit Sangoi <108111315+vinitsangoi@users.noreply.github.com>
2023-01-23 20:53:21 +0530

    Update Ratings_Analysis.sql

--- Ratings_Analysis.sql
+++ Ratings_Analysis.sql
@@ -19,31 +19,31 @@ This SQL queries analyzes the ratings given by customers to restaurants. This he
 
 --- Q27) Ratings given by customer for restaurants
 SELECT 
-		b.consumer_id,
-		a.name,
-		b.overall_rating,
-		b.food_rating,
-		b.service_rating
-FROM restaurants as a 
+	   b.consumer_id,
+	   a.name,
+	   b.overall_rating,
+	   b.food_rating,
+	   b.service_rating
+FROM       restaurants as a 
 INNER JOIN customer_ratings as b
-ON a.restaurant_id = b.restaurant_id
-ORDER BY b.restaurant_id
+ON         a.restaurant_id = b.restaurant_id
+ORDER BY   b.restaurant_id
 
 
 --- Q28) Average ratings of each restaurant including it's cuisine type
 SELECT 
-		a.name,
-		ROUND(AVG(b.overall_rating),2)as overall_Rating,
-		ROUND(AVG(b.food_rating),2)as food_rating,
-		ROUND(AVG(b.service_rating),2)as service_rating,
-		c.cuisine
-FROM restaurants as a 
+	   a.name,
+	   ROUND(AVG(b.overall_rating),2)as overall_Rating,
+	   ROUND(AVG(b.food_rating),2)as food_rating,
+	   ROUND(AVG(b.service_rating),2)as service_rating,
+	   c.cuisine
+FROM       restaurants as a 
 INNER JOIN customer_ratings as b
-ON a.restaurant_id = b.restaurant_id
+ON         a.restaurant_id = b.restaurant_id
 INNER JOIN restaurant_cuisines AS c
-ON a.restaurant_id = c.restaurant_id
-GROUP BY a.name,c.cuisine
-ORDER BY a.name
+ON         a.restaurant_id = c.restaurant_id
+GROUP BY   a.name,c.cuisine
+ORDER BY   a.name
 
 --- Q29) Creating new columns for sentiment analysis
 ALTER TABLE customer_ratings ADD COLUMN overall_senti Varchar(50)
@@ -57,206 +57,205 @@ SELECT * FROM customer_ratings
 
 UPDATE customer_ratings
 SET overall_senti = CASE WHEN overall_rating = 0 then 'Negative'
-					 	 WHEN overall_rating = 1 then 'Neutral'	
-					 	 WHEN overall_rating = 2 then 'Positive'
-					  	 END
+			 WHEN overall_rating = 1 then 'Neutral'	
+			 WHEN overall_rating = 2 then 'Positive'
+			 END
 WHERE overall_senti is null
 
 UPDATE customer_ratings
-SET food_senti = CASE 	 WHEN food_rating = 0 then 'Negative'
-					 	 WHEN food_rating = 1 then 'Neutral'	
-					 	 WHEN food_rating = 2 then 'Positive'
-					  	 END
+SET food_senti = CASE WHEN food_rating = 0 then 'Negative'
+		      WHEN food_rating = 1 then 'Neutral'	
+		      WHEN food_rating = 2 then 'Positive'
+		      END
 WHERE food_senti is null
 
 UPDATE customer_ratings
 SET service_senti = CASE WHEN service_rating = 0 then 'Negative'
-					 	 WHEN service_rating = 1 then 'Neutral'	
-					 	 WHEN service_rating = 2 then 'Positive'
-					  	 END
+			 WHEN service_rating = 1 then 'Neutral'	
+			 WHEN service_rating = 2 then 'Positive'
+			 END
 WHERE service_senti is null
 
 
 --- Q31) Conduct a sentimental analysis of total count of customers
 CREATE VIEW overall AS (
 SELECT 
-		overall_senti,
-		count(consumer_id) as total_customers
+	overall_senti,
+	count(consumer_id) as total_customers
 FROM 	customer_ratings
 GROUP BY overall_senti
 
 
 CREATE VIEW food AS (
 SELECT 
-		food_senti,
-		count(consumer_id) as total_customers
+	food_senti,
+	count(consumer_id) as total_customers
 FROM 	customer_ratings
 GROUP BY food_senti
 
 
 CREATE VIEW service AS (
 SELECT 
-		service_senti,
-		count(consumer_id) as total_customers
+	service_senti,
+	count(consumer_id) as total_customers
 FROM 	customer_ratings
 GROUP BY service_senti
 
 
 SELECT
-		a.overall_senti as Sentiment,
-		a.total_customers as Overall_Rating,
-		b.total_customers as food_Rating,
-		c.total_customers as service_Rating
-FROM overall as a
+	   a.overall_senti as Sentiment,
+	   a.total_customers as Overall_Rating,
+	   b.total_customers as food_Rating,
+	   c.total_customers as service_Rating
+FROM       overall as a
 INNER JOIN food as b
-ON a.overall_senti = b.food_senti
+ON         a.overall_senti = b.food_senti
 INNER JOIN service as c
-ON a.overall_senti = c.service_senti
+ON         a.overall_senti = c.service_senti
 
 
 --- Q32) List of Customers visiting local or outside restaurants
 
 SELECT 
-		a.consumer_id,
-		b.city as customer_city,
-		c.name,
-		c.city as restaurant_city,
-		a.overall_senti,
-		a.food_senti,
-		a.service_senti,
-		CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
-FROM customer_ratings as a
+	   a.consumer_id,
+	   b.city as customer_city,
+	   c.name,
+	   c.city as restaurant_city,
+	   a.overall_senti,
+	   a.food_senti,
+	   a.service_senti,
+	   CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
+FROM       customer_ratings as a
 INNER JOIN customer_details as b
-ON a.consumer_id = b.consumer_id
+ON         a.consumer_id = b.consumer_id
 INNER JOIN restaurants as c
-ON a.restaurant_id = c.restaurant_id
+ON         a.restaurant_id = c.restaurant_id
 
 
 --- Q33) Count of customers visiting local and outside restaurants
 
 SELECT 
-		Location_preference,
-		count(*) as total_customers,
-		count( distinct id) as distinct_customers
-FROM 	(SELECT 
-				a.consumer_id as id,
-				b.city as customer_city,
-				c.name,
-				c.city as restaurant_city,
-				a.overall_senti,
-				a.food_senti,
-				a.service_senti,
-				CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
-			FROM customer_ratings as a
-			INNER JOIN customer_details as b
-			ON a.consumer_id = b.consumer_id
-			INNER JOIN restaurants as c
-			ON a.restaurant_id = c.restaurant_id )	as cte
-GROUP BY 	Location_preference				
+	Location_preference,
+	count(*) as total_customers,
+	count( distinct id) as distinct_customers
+FROM 	(    SELECT 
+			 a.consumer_id as id,
+			 b.city as customer_city,
+			 c.name,
+			 c.city as restaurant_city,
+			 a.overall_senti,
+			 a.food_senti,
+			 a.service_senti,
+			 CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
+	      FROM       customer_ratings as a
+	      INNER JOIN customer_details as b
+	      ON         a.consumer_id = b.consumer_id
+	      INNER JOIN restaurants as c
+	      ON         a.restaurant_id = c.restaurant_id ) as cte
+GROUP BY  Location_preference				
 		
 		
 --- Q34) Trend of customers visiting outside restaurants
 
 SELECT 
-		customer_id,
-		customer_city,
-		restaurant_city,
-		concat_ws(' - ',customer_city , restaurant_city) as direction,
-		restaurant_name		
-FROM 	(SELECT 
-				a.consumer_id as customer_id,
-				b.city as customer_city,
-				c.name as restaurant_name,
-				c.city as restaurant_city,
-				a.overall_senti,
-				a.food_senti,
-				a.service_senti,
-				CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
-			FROM customer_ratings as a
-			INNER JOIN customer_details as b
-			ON a.consumer_id = b.consumer_id
-			INNER JOIN restaurants as c
-			ON a.restaurant_id = c.restaurant_id )	as cte
-WHERE 	Location_preference = 'Outside'
+	customer_id,
+	customer_city,
+	restaurant_city,
+	concat_ws(' - ',customer_city , restaurant_city) as direction,
+	restaurant_name		
+FROM 	(  SELECT 
+		     a.consumer_id as customer_id,
+		     b.city as customer_city,
+		     c.name as restaurant_name,
+		     c.city as restaurant_city,
+	             a.overall_senti,
+	             a.food_senti,
+	             a.service_senti,
+	  CASE WHEN  b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
+	  FROM       customer_ratings as a
+	  INNER JOIN customer_details as b
+	  ON         a.consumer_id = b.consumer_id
+	  INNER JOIN restaurants as c
+	  ON         a.restaurant_id = c.restaurant_id )	as cte
+WHERE  Location_preference = 'Outside'
 
 		
 		
 --- Q35) Count of direction trend from above query
 
 SELECT
-		direction,
-		count(customer_id) as total_customers
+	direction,
+	count(customer_id) as total_customers
 
-FROM  (SELECT 
+FROM    (  SELECT 
 		customer_id,
 		customer_city,
 		restaurant_city,
 		concat_ws(' - ',customer_city , restaurant_city) as direction,
 		restaurant_name
 		
-		FROM 	(SELECT 
-					a.consumer_id as customer_id,
-					b.city as customer_city,
-					c.name as restaurant_name,
-					c.city as restaurant_city,
-					a.overall_senti,
-					a.food_senti,
-					a.service_senti,
-				CASE WHEN b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
-				FROM customer_ratings as a
-			 	INNER JOIN customer_details as b
-				ON a.consumer_id = b.consumer_id
-				INNER JOIN restaurants as c
-				ON a.restaurant_id = c.restaurant_id )	as cte
-		WHERE 	Location_preference = 'Outside' ) cte2
+	    FROM    (  SELECT 
+				   a.consumer_id as customer_id,
+				   b.city as customer_city,
+				   c.name as restaurant_name,
+				   c.city as restaurant_city,
+				   a.overall_senti,
+				   a.food_senti,
+				   a.service_senti,
+			CASE WHEN  b.city = c.city THEN 'Local' ELSE 'Outside' END as Location_preference
+			FROM       customer_ratings as a
+			INNER JOIN customer_details as b
+			ON         a.consumer_id = b.consumer_id
+			INNER JOIN restaurants as c
+			ON         a.restaurant_id = c.restaurant_id )	as cte
+	     WHERE   Location_preference = 'Outside' ) cte2
 GROUP BY direction
 
 
 --- Q36) Cuisine preferences vs cuisine consumed
 
 SELECT 
-		a.consumer_id,
-		string_agg(b.preferred_cuisine,',') as customer_preferences,
-		d.name,
-		c.cuisine as restaurant_cuisine
-FROM customer_ratings as a
+	   a.consumer_id,
+	   string_agg(b.preferred_cuisine,',') as customer_preferences,
+	   d.name,
+	   c.cuisine as restaurant_cuisine
+FROM       customer_ratings as a
 INNER JOIN customer_preference as b
-ON a.consumer_id = b.consumer_id
+ON         a.consumer_id = b.consumer_id
 INNER JOIN restaurant_cuisines as c
-ON a.restaurant_id = c.restaurant_id
+ON         a.restaurant_id = c.restaurant_id
 INNER JOIN restaurants as d
-ON a.restaurant_id = d.restaurant_id
-GROUP BY a.consumer_id, d.name, c.cuisine
+ON         a.restaurant_id = d.restaurant_id
+GROUP BY   a.consumer_id, d.name, c.cuisine
 
 
 --- Q37) Best restaurants for each cuisines by different ratings
 CREATE VIEW average_analysis as (
 SELECT 
-		a.name,
-		ROUND(AVG(b.overall_rating),2)as overall_Rating,
-		ROUND(AVG(b.food_rating),2)as food_rating,
-		ROUND(AVG(b.service_rating),2)as service_rating,
-		c.cuisine
-FROM restaurants as a 
+	   a.name,
+	   ROUND(AVG(b.overall_rating),2)as overall_Rating,
+	   ROUND(AVG(b.food_rating),2)as food_rating,
+	   ROUND(AVG(b.service_rating),2)as service_rating,
+	   c.cuisine
+FROM       restaurants as a 
 INNER JOIN customer_ratings as b
-ON a.restaurant_id = b.restaurant_id
+ON         a.restaurant_id = b.restaurant_id
 INNER JOIN restaurant_cuisines AS c
-ON a.restaurant_id = c.restaurant_id
-GROUP BY a.name,c.cuisine
-ORDER BY c.cuisine)
+ON         a.restaurant_id = c.restaurant_id
+GROUP BY   a.name,c.cuisine
+ORDER BY   c.cuisine)
 	
 
 CREATE VIEW best  as (
 SELECT 
-		cuisine,
-		First_value(name) OVER(partition by cuisine ORDER BY overall_rating desc) as best_for_overall,
-		First_value(name) OVER(partition by cuisine ORDER BY food_rating desc) as best_for_food,
-		First_value(name) OVER(partition by cuisine ORDER BY service_rating desc) as best_for_service
+     cuisine,
+     First_value(name) OVER(partition by cuisine ORDER BY overall_rating desc) as best_for_overall,
+     First_value(name) OVER(partition by cuisine ORDER BY food_rating desc) as best_for_food,
+     First_value(name) OVER(partition by cuisine ORDER BY service_rating desc) as best_for_service
 FROM average_analysis
 
-SELECT 
-		*
-FROM best
+SELECT   *
+FROM     best
 GROUP BY cuisine, best_for_overall, best_for_food, best_for_service
 ORDER BY cuisine	
 
